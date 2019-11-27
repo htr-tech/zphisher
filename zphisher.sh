db6a21dbbf997414c2dc48ab5e2969c0e656531a
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2019-11-27 13:48:24 +0600

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -12,11 +12,11 @@ trap 'printf "\n";stop;exit 1' 2
 
 dependencies() {
 
-command -v php > /dev/null 2>&1 || { echo >&2 "\e[0m\e[1;93m PHP is not installed ! Install it."; exit 1; }
-command -v curl > /dev/null 2>&1 || { echo >&2 "\e[0m\e[1;93m Curl is not installed ! Install it."; exit 1; }
-command -v wget > /dev/null 2>&1 || { echo >&2 "\e[0m\e[1;93m Wget is not installed ! Install it"; exit 1; }
-command -v ssh > /dev/null 2>&1 || { echo >&2 "\e[0m\e[1;93m Openssh is not installed ! Install it"; exit 1; }
-command -v unzip > /dev/null 2>&1 || { echo >&2 "\e[0m\e[1;93m Unzip is not installed ! Install it"; exit 1; }
+command -v php > /dev/null 2>&1 || { echo >&2 "PHP is not installed ! Install it."; exit 1; }
+command -v curl > /dev/null 2>&1 || { echo >&2 "Curl is not installed ! Install it."; exit 1; }
+command -v wget > /dev/null 2>&1 || { echo >&2 "Wget is not installed ! Install it"; exit 1; }
+command -v ssh > /dev/null 2>&1 || { echo >&2 "Openssh is not installed ! Install it"; exit 1; }
+command -v unzip > /dev/null 2>&1 || { echo >&2 "Unzip is not installed ! Install it"; exit 1; }
 
 }    
 banner() {
