6ea6c5fb43bf77f7265363d132dbb4b43a0fd311
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2019-12-02 11:48:36 +0600

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -347,6 +347,8 @@ printf "\n\e[96m[\e[0m\e[1;77m~\e[96m]\e[0m\e[1;96m Press Ctrl + C to exit.\e[0m
 printf "\e[1;93m\n"
 ssh -R 80:localhost:$port ssh.localhost.run 2>&1
 printf "\e[0m\n"
+found
+
 }
 start_l() {
 def_port="5555"
