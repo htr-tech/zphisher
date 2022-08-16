6af91db84b2f543d715f688c3060ea0aeda69f7a
ELQNAS DAYMON <110784685+elqnas-daymon-x1@users.noreply.github.com>
2022-08-16 23:46:16 +0200

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -167,7 +167,7 @@ banner() {
 		${ORANGE}      | |                                
 		${ORANGE}      |_|                ${RED}Version : ${__version__}
 
-		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by htr-tech (tahmid.rayat)${WHITE}
+		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by ELQNAS-DAYMON (tahmid.rayat)${WHITE}
 	EOF
 }
 
