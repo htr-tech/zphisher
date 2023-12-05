a9d6b804e1809b384367417944358ba2c3713ea6
Mohammad Sohrowardi <151482266+sohrowardi@users.noreply.github.com>
2023-12-05 08:50:21 +0600

    Refactor Zphisher Debian packaging script for readability and robustness

--- make-deb.sh
+++ make-deb.sh
@@ -1,51 +1,65 @@
 #!/bin/bash
+set -e
 
-# Make Deb Package for Zphisher (^.^)
-_PACKAGE=zphisher
-_VERSION=2.3.5
+# Variables
+_PACKAGE="zphisher"
+_VERSION="2.3.5"
 _ARCH="all"
 PKG_NAME="${_PACKAGE}_${_VERSION}_${_ARCH}.deb"
+BUILD_DIR="./build_env"
+SCRIPT_DIR="./scripts"
 
-if [[ ! -e "scripts/launch.sh" ]]; then
-        echo "lauch.sh should be in the \`scripts\` Directory. Exiting..."
-        exit 1
+# Check dependencies
+command -v dpkg-deb >/dev/null 2>&1 || { echo >&2 "dpkg-deb is required but not installed. Aborting."; exit 1; }
+
+# Check existence of launch.sh
+if [[ ! -e "${SCRIPT_DIR}/launch.sh" ]]; then
+    echo "launch.sh should be in the \`${SCRIPT_DIR}\` directory. Exiting..."
+    exit 1
 fi
 
-if [[ ${1,,} == "termux" || $(uname -o) == *'Android'* ]];then
-        _depend="ncurses-utils, proot, resolv-conf, "
-        _bin_dir="data/data/com.termux/files/"
-        _opt_dir="data/data/com.termux/files/usr/"
-        #PKG_NAME=${_PACKAGE}_${_VERSION}_${_ARCH}_termux.deb
+# Environment and Dependency Setup
+if [[ "${1,,}" == "termux" || $(uname -o) == *'Android'* ]]; then
+    _depend="ncurses-utils, proot, resolv-conf, "
+    _bin_dir="data/data/com.termux/files/usr/bin"
+    _opt_dir="data/data/com.termux/files/usr/opt/${_PACKAGE}"
 fi
 
 _depend+="curl, php, unzip"
 _bin_dir+="usr/bin"
 _opt_dir+="opt/${_PACKAGE}"
 
-if [[ -d "build_env" ]]; then rm -fr build_env; fi
-mkdir -p build_env
-mkdir -p ./build_env/${_bin_dir} ./build_env/$_opt_dir ./build_env/DEBIAN 
+# Directory Setup
+rm -rf "${BUILD_DIR}"
+mkdir -p "${BUILD_DIR}/${_bin_dir}" "${BUILD_DIR}/${_opt_dir}" "${BUILD_DIR}/DEBIAN"
 
-cat <<- CONTROL_EOF > ./build_env/DEBIAN/control
+# Control File Generation
+cat <<- CONTROL_EOF > "${BUILD_DIR}/DEBIAN/control"
 Package: ${_PACKAGE}
 Version: ${_VERSION}
 Architecture: ${_ARCH}
 Maintainer: @htr-tech
 Depends: ${_depend}
 Homepage: https://github.com/htr-tech/zphisher
-Description: An automated phishing tool with 30+ templates. This Tool is made for educational purpose only !
+Description: An automated phishing tool with 30+ templates. This Tool is made for educational purpose only!
 CONTROL_EOF
 
-cat <<- PRERM_EOF > ./build_env/DEBIAN/prerm
+cat <<- PRERM_EOF > "${BUILD_DIR}/DEBIAN/prerm"
 #!/bin/bash
-rm -fr $_opt_dir
+rm -fr ${_opt_dir}
 exit 0
 PRERM_EOF
 
-chmod 755 ./build_env/DEBIAN
-chmod 755 ./build_env/DEBIAN/{control,prerm}
-cp -fr scripts/launch.sh ./build_env/$_bin_dir/$_PACKAGE
-chmod 755 ./build_env/$_bin_dir/$_PACKAGE
-cp -fr .github/ .sites/ LICENSE README.md zphisher.sh ./build_env/$_opt_dir
-dpkg-deb --build ./build_env ${PKG_NAME}
-rm -fr ./build_env
+chmod -R 755 "${BUILD_DIR}/DEBIAN"
+chmod 755 "${BUILD_DIR}/DEBIAN"{/control,/prerm}
+
+# File Copying
+cp -fr "${SCRIPT_DIR}/launch.sh" "${BUILD_DIR}/${_bin_dir}/${_PACKAGE}"
+chmod 755 "${BUILD_DIR}/${_bin_dir}/${_PACKAGE}"
+cp -fr .github/ .sites/ LICENSE README.md zphisher.sh "${BUILD_DIR}/${_opt_dir}"
+
+# Build Debian Package
+dpkg-deb --build "${BUILD_DIR}" "${PKG_NAME}"
+
+# Clean up
+rm -rf "${BUILD_DIR}"
