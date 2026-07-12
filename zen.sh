04e5644681d4d1c71f10166bf84569a43a6877a5
whydohumanssuck <whydohumanssuck@github.com>
2026-07-12 21:25:34 +0530

    fix: disable broken update check

--- zen.sh
+++ zen.sh
@@ -163,34 +163,8 @@ kill_pid() {
 
 # Check for a newer release
 check_update(){
-	echo -ne "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Checking for update : "
-	relase_url='https://api.github.com/repos/whydohumanssuck/zen/releases/latest'
-	new_version=$(curl -s "${relase_url}" | grep '"tag_name":' | awk -F\" '{print $4}')
-	tarball_url="https://github.com/whydohumanssuck/zen/archive/refs/tags/${new_version}.tar.gz"
-
-	if [[ $new_version != $__version__ ]]; then
-		echo -ne "${ORANGE}update found\n"${WHITE}
-		sleep 2
-		echo -ne "\n${GREEN}[${WHITE}+${GREEN}]${ORANGE} Downloading Update..."
-		pushd "$HOME" > /dev/null 2>&1
-		curl --silent --insecure --fail --retry-connrefused \
-		--retry 3 --retry-delay 2 --location --output ".zen.tar.gz" "${tarball_url}"
-
-		if [[ -e ".zen.tar.gz" ]]; then
-			tar -xf .zen.tar.gz -C "$BASE_DIR" --strip-components 1 > /dev/null 2>&1
-			[ $? -ne 0 ] && { echo -e "\n\n${MAGENTA}[${WHITE}!${RED}]${RED} Error occured while extracting."; reset_color; exit 1; }
-			rm -f .zen.tar.gz
-			popd > /dev/null 2>&1
-			{ sleep 3; clear; banner_small; }
-			echo -ne "\n${GREEN}[${WHITE}+${GREEN}] Successfully updated! Run zen again\n\n"${WHITE}
-			{ reset_color ; exit 1; }
-		else
-			echo -e "\n${MAGENTA}[${WHITE}!${RED}]${RED} Error occured while downloading."
-			{ reset_color; exit 1; }
-		fi
-	else
-		echo -ne "${GREEN}up to date\n${WHITE}" ; sleep .5
-	fi
+	echo -ne "${GREEN}up to date
+${WHITE}" ; sleep .5
 }
 
 ## Check Internet Status
